15e6ecbf8b83ac57275fc379d4abc4948a15ec1d
EteoclesDinah <kamalaraidanuwar01@gmail.com>
2024-07-15 00:21:13 +0545

    description of homepage

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,16 @@ import './App.css';
 const Home = () => {
     return(
         <div className="container">
-            
+            <div className="homeDescription">
+                <h1>Your Go-To Web Scrapping tool!!</h1>
+                <p>Want to extract web content seamlessly?</p>
+                <p>Look no Further!</p>
+                <p>Harness the power of web scraping to collect data efficiently.<br /> Whether you're gathering information for research,<br />
+                 monitoring trends, or compiling useful resources, <br />our tool simplifies the process for you.</p>
+
+            </div>
+
+            <div className="scrappingContent"></div>
         </div>
     );
 };
